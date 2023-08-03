2d3680489e14cadbf4c5044c0b785b84e517a592
Nguyen Mai Huy <huynm21@fpt.edu.vn>
2023-08-03 07:01:00 -0400

    Update somethings
    Signed-off-by: Nguyen Mai Huy <huynm21@fpt.edu.vn>

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,26 @@
+import React, { useContext, useState  } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Budget = () => {
+    const { budget } = useContext(AppContext);
+    const [name, setName] = useState('');
+    const [cost, setCost] = useState('');
+    const expense = {
+        name: name,
+        cost: 10,
+    };
+    return (
+        <div className='form-inline alert alert-secondary'>
+            <label for='bud'>Budget: £</label>
+            <input
+                type='number'
+                id='cost'
+                value={budget}
+                style={{ marginLeft: '2rem' , size: 10}}
+                onChange={(event) => setCost(event.target.value)}>
+            </input>
+        </div>
+    );
+};
+
+export default Budget;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,1 +1,56 @@
+import React, { useContext, useState  } from 'react';
+//import { TiDelete } from 'react-icons/ti';
+import { AppContext } from '../context/AppContext';
+
+const ExpenseItem = (props) => {
+    const { dispatch,remaining } = useContext(AppContext);
+    const [cost, setCost] = useState('');
+
+//    const handleDeleteExpense = () => {
+//        dispatch({
+//            type: 'DELETE_EXPENSE',
+//            payload: props.id,
+//        });
+//    };
+
+    const increaseAllocation = (name) => {
+        if(cost > remaining) {
+            alert("The value cannot exceed remaining funds  £"+remaining);
+            setCost("");
+            return;
+        }
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        });
+
+    }
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+
+    }
+    return (
+        <tr>
+        <td>{props.name}</td>
+        <td>£{props.cost}</td>
+        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><button onClick={event=> decreaseAllocation(props.name)}>-</button></td>
+        </tr>
+    );
+};
+
+export default ExpenseItem;
 
