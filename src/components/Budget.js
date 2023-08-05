d13523e6f67eba104eed5a6bb0ca5adbfadd8d87
huynm21 <140838251+huynm21@users.noreply.github.com>
2023-08-05 12:33:00 +0700

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,23 +1,44 @@
-import React, { useContext, useState  } from 'react';
+import React, { useContext   } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    const [name, setName] = useState('');
-    const [cost, setCost] = useState('');
-    const expense = {
-        name: name,
-        cost: 10,
+    const {budget, dispatch } = useContext(AppContext);
+    const { expenses } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+    const budgetChange = (event) => {
+        if(budget > 20000) {
+            alert("The value cannot exceed 20000  £");
+            dispatch({
+                type: 'SET_BUDGET',
+                payload: budget,
+            });
+            return;
+        }else if ( budget<totalExpenses){
+            alert("The value cannot exceed ower than spending");
+            dispatch({
+                type: 'SET_BUDGET',
+                payload: budget,
+            });
+            return;
+        }
+        let sign = (event.target.value-budget);
+        dispatch({
+            type: sign>0 ? 'INC_BUDGET': 'DEC_BUDGET',
+            payload: budget,
+        });
     };
+
     return (
         <div className='form-inline alert alert-secondary'>
             <label for='bud'>Budget: £</label>
             <input
                 type='number'
-                id='cost'
+                id='budget'
                 value={budget}
                 style={{ marginLeft: '2rem' , size: 10, width: '128px'}}
-                onChange={(event) => setCost(event.target.value)}>
+                onChange={budgetChange}>
             </input>
         </div>
     );
